54c9b610b9dacbcb5f282da8234381692fa7de1a
BeningShum <119930401+BeningShum@users.noreply.github.com>
2022-12-14 18:04:09 +0800

    Update 核平滑.py

--- 案例实战代码/T2/核平滑.py
+++ 案例实战代码/T2/核平滑.py
@@ -44,7 +44,7 @@ def KernelSmoothing(
     return yr
 
 
-data = pd.read_csv("C:\\Users\\16548\\Desktop\\导论作业\\T2\\insurance.csv")
+data = pd.read_csv(".../insurance.csv")
 y = data["charges"]
 x = data.drop(labels="charges", axis=1)
 x = pd.get_dummies(x, columns=["sex", "smoker", "region"])
